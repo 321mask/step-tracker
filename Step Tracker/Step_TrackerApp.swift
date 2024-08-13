3b810f69739af278914f670f82a05dc44623ac69
321mask <166810173+321mask@users.noreply.github.com>
2024-08-13 16:34:14 +0200

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Арсений Простаков on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
