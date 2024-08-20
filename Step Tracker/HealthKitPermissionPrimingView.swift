84dd2940d9bd29b6ee221e420fd2c42623ad3bab
321mask <166810173+321mask@users.noreply.github.com>
2024-08-20 13:36:35 +0200

    permission priming ui

---
+++ Step Tracker/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,44 @@
+//
+//  HealthKitPermissionPrimingView.swift
+//  Step Tracker
+//
+//  Created by Арсений Простаков on 20.08.2024.
+//
+
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    var description = """
+This app displays your step and weight
+data in interactive charts.
+
+You can also
+add new step or weight data to Apple Health from this app. Your data is private and secured.|
+"""
+    
+    var body: some View {
+        VStack(spacing: 130) {
+            VStack(alignment: .leading, spacing: 10) {
+                Image(.iconAppleHealth)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3), radius: 6)
+                    .padding(.bottom, 12)
+                Text("Apple Health Integration")
+                    .font(.title2).bold()
+                Text(description)
+                    .foregroundStyle(.secondary)
+            }
+            Button("Connect Apple Health") {
+                //do code
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
