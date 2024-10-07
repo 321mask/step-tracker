bd7a9e884a9e7eec2163625b59cde4abc1289642
321mask <166810173+321mask@users.noreply.github.com>
2024-10-07 14:32:22 +0200

    implement chart empty states

--- Step Tracker/Charts/WeightBarChart.swift
+++ Step Tracker/Charts/WeightBarChart.swift
@@ -45,34 +45,39 @@ struct WeightBarChart: View {
                     Text("Per Weekday (Last 28 Days)")
                         .font(.caption)
                 }
+                Spacer()
             }
-            Chart {
-                if let selectedData {
-                    RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
-                        .foregroundStyle(Color.secondary.opacity(0.3))
-                        .offset (y: -10)
-                        .annotation(position: .top,
-                                    spacing: 0,
-                                    overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotationView }
+            if chartData.isEmpty {
+                ChartEmptyView(systemImageName: "chart.bar", title: "No Data", discription: "There is no weight data from the Health App.")
+            } else {
+                Chart {
+                    if let selectedData {
+                        RuleMark(x: .value("Selected Data", selectedData.date, unit: .day))
+                            .foregroundStyle(Color.secondary.opacity(0.3))
+                            .offset (y: -10)
+                            .annotation(position: .top,
+                                        spacing: 0,
+                                        overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) { annotationView }
+                    }
+                    ForEach(chartData) { weight in
+                        BarMark(x: .value("Date", weight.date),
+                                y: .value("Weight change", weight.value))
+                        .foregroundStyle(weight.value >= 0 ? Color.indigo.gradient : Color.mint.gradient)
+                    }
                 }
-                ForEach(chartData) { weight in
-                    BarMark(x: .value("Date", weight.date),
-                            y: .value("Weight change", weight.value))
-                    .foregroundStyle(weight.value >= 0 ? Color.indigo.gradient : Color.mint.gradient)
+                .frame(height: 150)
+                .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
+                .chartXAxis {
+                    AxisMarks(values: .stride(by: .day)) {
+                        AxisValueLabel(format: .dateTime.weekday(), centered: true)
+                    }
                 }
-            }
-            .frame(height: 150)
-            .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
-            .chartXAxis {
-                AxisMarks(values: .stride(by: .day)) {
-                    AxisValueLabel(format: .dateTime.weekday(), centered: true)
-                }
-            }
-            .chartYAxis {
-                AxisMarks { value in
-                    AxisGridLine()
-                        .foregroundStyle(Color.secondary.opacity(0.3))
-                    AxisValueLabel()
+                .chartYAxis {
+                    AxisMarks { value in
+                        AxisGridLine()
+                            .foregroundStyle(Color.secondary.opacity(0.3))
+                        AxisValueLabel()
+                    }
                 }
             }
         }
